39ad82ccb78551e012c6a475d78399d1811465d1
peendrive <leonardo.dominguez@alumnos.frm.utn.edu.ar>
2020-05-19 19:00:24 -0300

    comentarios y checkeo de numero invalido

--- tp5.py
+++ tp5.py
@@ -1,24 +1,36 @@
-def kaprekar(numero,bucles):
-    
-    largoFaltante=4-len(numero)
+def kaprekar(numero,bucles): 
+    #el metodo kaprekar usará las ventajas de la recursividad
+    largoFaltante=4-len(numero) 
+    #calculamos el largo faltante del string para cumplir con las 4 cifras
     invertir(numero)
-    for x in range(0, largoFaltante):
-        numero+="0"
+    #invertimos el numero para hacer mas corta la concatenación
+    if(largoFaltante!=0):
+        for x in range(0, largoFaltante):
+            numero+="0"
     inv=int(invertir(numero))
     numero=int(numero)
-    if(inv-numero==6174 or numero-inv==6174):
-        print("funciono"+bucles)
-        pass 
+    #convertimos los strings en numeros
+    if(numero-inv==0):
+        print("numero Invalido:")
+        pass
+    #nos aseguramos de que no sea un numero invalido como 1111
     else:
-        if(inv-numero>0):
-            bucles=bucles+1
-            kaprekar(str(inv-numero),bucles)
-            pass
-        else:       
-            bucles=bucles+1
-            kaprekar(str(numero-inv), bucles)
-            pass
-    pass
+        if(inv-numero==6174 or numero-inv==6174):
+            print("funciono"+bucles)
+            pass 
+        #checkeamos que no sea la constante de kapekar
+        else:
+            #si no lo es correremos nuevamente el bucle, sumando uno a la cantidad de iteraciones.
+            if(inv-numero>0):
+                bucles=bucles+1
+                kaprekar(str(inv-numero),bucles)
+                
+                pass
+            else:       
+                bucles=bucles+1
+                kaprekar(str(numero-inv), bucles)
+                pass
+        pass
         
 pass
 
