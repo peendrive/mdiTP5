5f2b9fd1e9c1485bdec57a2b17d747f58b1b794b
peendrive <leonardo.dominguez@alumnos.frm.utn.edu.ar>
2020-05-20 12:09:24 -0300

    Unificacion en tp 5

--- tp 5.py
+++ tp 5.py
@@ -1,55 +1,89 @@
 # 
 #  elif: opcionMenu == "2":
-
 import os
 from io import open  # libreria
 
 archivo_texto = open("numeross.txt", "r") # r para leer el archivo
-
+lineas=archivo_texto.readlines()
 texto = archivo_texto.read() # se abre el archivo y se lee
 
 archivo_texto.close() # cerramos el archivo
 
 print(texto)
 
-"""
+def invertir(s):
+    return s[::-1]
+def ordenar(s):
+    a=sorted(s)
+    s=''.join(a)
+    return s
+def kaprekar(numero,bucles): 
+    #el metodo kaprekar usará las ventajas de la recursividad
+    largoFaltante=4-len(numero) 
+    #calculamos el largo faltante del string para cumplir con las 4 cifras
+    invertir(numero)
+    #invertimos el numero para hacer mas corta la concatenación
+    if(largoFaltante!=0):
+        for x in range(0, largoFaltante):
+            numero+="0"
+    numero=ordenar(numero)
+
+    inv=int(numero)
+    numero=int(invertir(numero))
+    #convertimos los strings en numeros
+    if(numero-inv==0):
+        print("numero Invalido:")
+        pass
+    #nos aseguramos de que no sea un numero invalido como 1111
+    
+    else:
+        if(inv-numero==6174 or numero-inv==6174):
+            bucles=bucles+1
+            print("funciono:"+' la cantidad de bucles fue de : '+str(bucles))
+            pass 
+        #checkeamos que no sea la constante de kapekar
+        else:
+            #si no lo es correremos nuevamente el bucle, sumando uno a la cantidad de iteraciones.
+            if(inv-numero>0):
+                bucles=bucles+1
+                aux=inv-numero
+                kaprekar(str(aux),bucles)
+                
+                pass
+            else:       
+                bucles=bucles+1
+                aux=numero-inv
+                kaprekar(str(aux), bucles)
+                pass
+        pass
+        
+pass 
+#codigo del otro archivo
+os.system('clear')
+
 def menu():
-    os.system('cls')
+
     print(" selecciona una opcion ")
     print(" \t1 - 1ra opcion ") # opcion de ingresar un numero 
     print(" \t2 - 2da opcion ") # opcion atraves de un archivo txt
-
 while True:
+    opcionMenu=0
     menu()
-    opcionMenu = input( " ingresa el valor ")
-
-
-def ascendente(numero) :
-    return int(''.join(sorted(str(num-inv))))
-
-def descendente(numero) :
-   return int(''.join(sorted(str(num-inv))[::-1]))
-
-
-
-while True:
-   
-    numero = texto
-    print(descendente(numero), "-", ascendente(numero), "=", descendente(numero)-ascendente(numero))
-    numero = descendente(numero) - ascendente(numero)
-
-
-    if i not in tex:
-        tex.append(num-inv)
+    opcionMenu = input( " Ingrese la opcion: ")
+    if(opcionMenu==1):
+        print("Ingrese un numero :")
+        numero=input()
+        kaprekar(numero,0) 
+        pass
     else:
-        if tex.index(num-inv) == len(tex)-1:
-            tex = []
-            tex.append(num-inv)
-            loop = 'constant'
-        else:
-            tex = tex[tex.index(numeros):]
-        break
-
-print('Kaprekar', loop, 'reached:', tex)
-"""
-
+      # cantidad=int(texto.readline())
+       
+        count=0
+        for line in lineas: 
+          
+            print(line.strip())
+            if(count>0):
+                kaprekar(line.strip(),0)
+                pass
+            count += 1
+        pass
