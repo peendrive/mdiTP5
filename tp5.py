6dc9272d4d1401644e640bc3ebec9121fc4cedd0
peendrive <leonardo.dominguez@alumnos.frm.utn.edu.ar>
2020-05-19 21:25:02 -0300

    Corregí error en el algoritmo, faltaba ordenar los numeros

--- tp5.py
+++ tp5.py
@@ -1,3 +1,9 @@
+def invertir(s):
+    return s[::-1]
+def ordenar(s):
+    a=sorted(s)
+    s=''.join(a)
+    return s
 def kaprekar(numero,bucles): 
     #el metodo kaprekar usará las ventajas de la recursividad
     largoFaltante=4-len(numero) 
@@ -7,35 +13,40 @@ def kaprekar(numero,bucles):
     if(largoFaltante!=0):
         for x in range(0, largoFaltante):
             numero+="0"
-    inv=int(invertir(numero))
-    numero=int(numero)
+    numero=ordenar(numero)
+
+    inv=int(numero)
+    numero=int(invertir(numero))
     #convertimos los strings en numeros
     if(numero-inv==0):
         print("numero Invalido:")
         pass
     #nos aseguramos de que no sea un numero invalido como 1111
+    
     else:
         if(inv-numero==6174 or numero-inv==6174):
-            print("funciono"+bucles)
+            bucles=bucles+1
+            print("funciono:"+' la cantidad de bucles fue de : '+str(bucles))
             pass 
         #checkeamos que no sea la constante de kapekar
         else:
             #si no lo es correremos nuevamente el bucle, sumando uno a la cantidad de iteraciones.
             if(inv-numero>0):
                 bucles=bucles+1
-                kaprekar(str(inv-numero),bucles)
+                aux=inv-numero
+                kaprekar(str(aux),bucles)
                 
                 pass
             else:       
                 bucles=bucles+1
-                kaprekar(str(numero-inv), bucles)
+                aux=numero-inv
+                kaprekar(str(aux), bucles)
                 pass
         pass
         
 pass
 
-def invertir(s):
-    return s[::-1]
+
 
 bucles=0
 print("Ingrese un numero :")
